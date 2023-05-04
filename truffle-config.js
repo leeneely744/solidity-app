cd4bb6c502363b477b5862a129aa4634d5ce52e2
kohei sano <nlee9337@gmail.com>
2023-05-04 22:22:00 +0900

    Hello World

---
+++ truffle-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545, // Change this to the port your Ganache instance is running on
+      network_id: "*" // Match any network id
+    },
+  },
+  compilers: {
+    solc: {
+      version: "^0.8.0"
+    }
+  }
+};
