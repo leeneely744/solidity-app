6d808e6a8fd837f948f5e63af1cefbbef1460842
kohei sano <nlee9337@gmail.com>
2023-05-05 12:22:49 +0900

    Create web3 base

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import HelloWorld from './contracts/HelloWorld.json';
 
-function App() {
+const App = () => {
+  const [web3, setWeb3] = useState(null);
+  const [accounts, setAccounts] = useState([]);
+  const [contract, setContract] = useState(null);
+
+  useEffect(() => {
+    const init = async () => {
+      // create web3 instance
+      // http://localhost:7545 はGanacheのデフォルトのURL
+      const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+      setWeb3(web3);
+
+      // get accounts
+      const fetchedAccounts  = await web3.eth.getAccounts();
+      setAccounts(fetchedAccounts );
+
+      // create instance of HelloWorld contract
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = HelloWorld.networks[networkId];
+      const instance = new web3.eth.Contract(
+        HelloWorld.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+      setContract(instance);
+    };
+
+    if (!web3) {
+      init();
+    }
+  }, [web3]);
+
+  // ここにアプリのロジックやUIを追加する
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello, World!</h1>
     </div>
   );
-}
+};
 
 export default App;
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 const HelloWorld = artifacts.require("HelloWorld");
 
 module.exports = function(deployer) {
-    deployer.deploy(HelloWorld, "Lee");
+    deployer.deploy(HelloWorld, "Lee").then(() => {
+        const contractsPath = path.join(__dirname, '..', 'client', 'src', 'contracts');
+        fs.mkdirSync(contractsPath, { recursive: true });
+
+        fs.copyFileSync(
+            path.join(__dirname, '..', 'build', 'contracts', 'HelloWorld.json'),
+            path.join(contractsPath, 'HelloWorld.json')
+        );
+    });
 }
